d578270da2cf3430d10d313b8ff4624a6b2729f7
Michael Serajnik <ms@mserajnik.at>
2018-08-29 18:23:01 +0200

    Add experimental Electron support

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@ module.exports = {
   configureWebpack: {
     resolve: {
       extensions: ['.js', '.json', '.vue', '.scss']
-    }
+    },
+    entry: (process.env.BUILD_FOR_ELECTRON === 'true')
+      ? { app: './src/main-electron.js' }
+      : { app: './src/main.js' }
   }
 }
